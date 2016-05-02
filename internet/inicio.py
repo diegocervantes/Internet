58de943d0da45b5833950089ff6e165678463f03
Valentino <dany.diaz@ucsp.edu.pe>
2016-05-01 23:13:40 -0500

    Added files via upload

--- internet/inicio.py
+++ internet/inicio.py
@@ -2,6 +2,26 @@
 #!/usr/bin/python
 print("Content-Type: text/html\n")
 
+etiquetas = ["INICIO","ASISTENCIAS","ADMIN"]
+enlaces = ["inicio.py","asistencias.py","admin.py"]
+sns = ["sns/logo1.png","sns/logo2.png","sns/logo3.png","sns/logo4.png"]
+imagenes = ["inicio/imagen3.png","inicio/imagen4.jpg","inicio/imagen5.jpg"]
+
+descripcion = """Inigo Solutions is a leading global investment banking, securities and investment management firm that provides a wide range of financial services 
+		to a substantial and diversified client base that includes corporations, financial institutions, governments and individuals. Founded in 1869, the firm is 
+		headquartered in Arequipa and maintains offices in all major financial centers around the world.
+		Open source solutions have changed the way people are thinking about software and hardware alike.Inigo Solutions joined the open source community, the 
+		Eclipse Foundation. As a solutions member, we will migrate our open source Java library, GS Collections, which will be renamed Eclipse Collections. The 
+		migration will allow the open source community to contribute to and benefit from our work. The new foundation will work to drive forward collaboration and 
+		standards on common container technologies among the developer community in deploying cloud native applications."""
+
+subs = ["""We strive to provide best-in-class advice and execution excellence on the most complex transactions across products in order to help our clients grow.""",
+ 		"""We are focused on being a significant financier and provider of capital-raising services, which, in turn, enables our clients to achieve their st
+		rategic goals.""",
+ 		"""Our Technology, Media and Telecommunications group provides insights and services covering a wide range of industries from electronics to software 
+		to Internet to wireless and cable companies."""]
+
+
 print ("""
 <!DOCTYPE html>
 <html>
@@ -22,12 +42,17 @@ print("""
 	</div>
 	
 </header>
+""")
 
-<div class="topbar">
-    <div class="links-top">
-		<a href="inicio.html"  title="Inicio"> INICIO</a>
-		<a href="asistencias.py"  title="Asistencias"> ASISTENCIAS</a>
-		<a href="admin.py"  title="Administración"> ADMIN</a>
+print("""
+	<div class="topbar">
+    <div class="links-top">""")
+j = 0
+for i in etiquetas:
+	print("""<a href=""",enlaces[j],"""  title=""",i,"""> """,i,"""</a>""")
+	j = j+1
+print("""
+
     </div>
 </div>
 
@@ -37,46 +62,31 @@ print("""
 		<div class="img-central">
 			<img src ="inicio/imagen.jpg" alt="imagen">	
 		</div>
-		<p>Inigo Solutions is a leading global investment banking, securities and investment management firm that provides a wide range of financial services 
-		to a substantial and diversified client base that includes corporations, financial institutions, governments and individuals. Founded in 1869, the firm is 
-		headquartered in Arequipa and maintains offices in all major financial centers around the world.
-		Open source solutions have changed the way people are thinking about software and hardware alike.Inigo Solutions joined the open source community, the 
-		Eclipse Foundation. As a solutions member, we will migrate our open source Java library, GS Collections, which will be renamed Eclipse Collections. The 
-		migration will allow the open source community to contribute to—and benefit from—our work. The new foundation will work to drive forward collaboration and 
-		standards on common container technologies among the developer community in deploying cloud native applications.</p>
+		<p>
+		""", descripcion, """</p>
 		
-	</div>
+	</div>""")
 
+j =0 
+for p in imagenes:
+	print("""
 	<div class="inicio-sub">
 		<div class="sub-img">
-			<img src="inicio/imagen3.png" alt="imagen" >
+			<img src=""",p,""" alt="imagen" >
 		</div>
-		<p >We strive to provide best-in-class advice and execution excellence on the most complex transactions across products in order to help our clients grow.</p>
+		<p >""", subs[j] ,"""</p>
 	</div>
+	""")
+	j = j+1
 
-	<div class="inicio-sub">
-		<div class="sub-img">
-			<img src="inicio/imagen4.jpg" alt="imagen" >
-		</div>
-		<p>We are focused on being a significant financier and provider of capital-raising services, which, in turn, enables our clients to achieve their st
-		rategic goals.</p>
-	</div>
-
-	<div class="inicio-sub">
-		<div class="sub-img">
-			<img src="inicio/imagen5.jpg" alt="imagen" >
-		</div>
-		<p>Our Technology, Media and Telecommunications group provides insights and services covering a wide range of industries—from electronics to software 
-		to Internet to wireless and cable companies.
-	</div>
-	
+print("""	
 </div>
 <div class="bottomBar">
 	<div class="redes" >
-		<a href="#"><img src="sns/logo1.png" alt="sns" ></a>
-		<a href="#"><img src="sns/logo2.png" alt="sns" ></a>
-		<a href="#"><img src="sns/logo3.png" alt="sns" ></a>
-		<a href="#"><img src="sns/logo4.png" alt="sns" ></a>
+	""")
+for i in sns:
+	print("""<a href="#"><img src=""",i,""" " alt="sns" ></a>""")
+print("""
 	</div>
 </div>
 </body>

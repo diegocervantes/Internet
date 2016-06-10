801f90232dbcb00f543e940ce8fe8294ff28a40d
diegocervantes <diego.cervantes@ucsp.edu.pe>
2016-06-10 03:53:57 -0500

    formulario

--- xampp/htdocs/internet/admin.py
+++ xampp/htdocs/internet/admin.py
@@ -2,11 +2,23 @@
 #!/usr/bin/python
 
 import mihtml
+import cgi
+import cgitb; cgitb.enable()
+import mysql.connector
+import re
+
 
 print("Content-Type: text/html\n")
 
-t1 = ["Agregar Area","Quitar Area", "Editar Horarios"]
+db= mysql.connector.connect(user='root', password='',
+                              host='127.0.0.1',
+                              database='inigo')
+
+
+
+t1 = ["Agregar Area","Quitar Area", "Cerrar Sesion"]
 t2 = ["Agregar Empleado", "Quitar Empleado", "Ver Empleado"]
+t3 = ["Area","Id","Encargado"]
 
 mihtml.head("Admin")
 mihtml.header()
@@ -14,25 +26,48 @@ mihtml.topbar()
 
 print"""
 <div class="contenido">
-    <table class="panel-admin">
-    <tr>
-        <td>Administrar Areas</td>
-        <td>Administrar Empleados </td>
-    </tr>
-    """
+    <div class="subdivision">
 
-j = 0
+        <div class="formulario-header">
+            <p>Administrar Areas</p>
+        </div>
+        """
 for i in t1:
     print"""
-        <tr>
+        <p>
             <td><a href="#" class="boton-admin">""",i,"""</a></td>
-            <td><a href="#" class="boton-admin">""",t2[j],"""</a></td>
-        </tr>
+        </p>
         """
-    j = j+1
+print"""
+        </table>
+    </div>
+"""
 
 print"""
-    </table>
+    <div class="subdivision2">
+        <div class="formulario-header">
+            <p>Input Form</p>
+        </div>
+            <form class="formulario-container">
+                <br>
+                <p>      
+                    <label class="formulario-text-grey">Nombre:</label>
+                    <input class="formulario-input" type="text" required="">
+                </p>
+                <p>      
+                    <label class="formulario-text-grey">Contrasena (Necesario letra mayuscula y dos signos):</label>
+                    <input class="formulario-input" type="text" required="">
+                </p>
+                <p>      
+                    <label class="formulario-text-grey">Fecha de Nacimineto (yyyy-mm-dd):</label>
+                    <input class="formulario-input" style="resize:none"></input> 
+                </p>
+                <p>
+                <button class="btn" type="button">Enviar</button>
+                </p>
+
+            </form>
+    </div>    
 </div>
-"""
+""" #si quieres poner textarea solo lo reemplazas por el input osea <textarea>balblaablab</textarea>
 mihtml.bottombar()

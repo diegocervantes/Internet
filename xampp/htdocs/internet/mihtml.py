99a0ff80ad5dd3d6e77da34682767d9ea9ec5feb
Valentino <dany.diaz@ucsp.edu.pe>
2016-06-23 19:59:40 -0500

    Update mihtml.py

--- xampp/htdocs/internet/mihtml.py
+++ xampp/htdocs/internet/mihtml.py
@@ -8,6 +8,26 @@ def head(nombre):
     <html>
     <head>
     <title>iniGO - """,nombre,"""</title>
+    <script type="text/javascript" src="jquery.min.js"></script> 
+    <script type="text/javascript"> 
+    function getData(){
+            var parametros = {
+                    "usuario" : document.getElementById('usuario').value,
+                    "password" : document.getElementById('password').value,
+            };
+            $.ajax({
+                    data:  parametros,
+                    url:   'insertar.py',
+                    type:  'get',
+                    beforeSend: function () {
+                            $("#contenido").html("Procesando, espere por favor...");
+                    },
+                    success:  function (response) {
+                            $("#contenido").html(response);
+                    }
+            });
+    }
+    </script> 
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" >
     <meta name="description" content="iniGO Solutions">
     <meta name="keywords" content="lista, asistencia, inigo, solution">
@@ -16,11 +36,18 @@ def head(nombre):
     """
 
 def login():
-    print """
-    <div class="login">
-        <input type="submit" name="Iniciar sesion" value="Iniciar Sesion">
-    </div>
-    """
+	print	"""
+	<div class = "login">
+		<form action="" method="post" name="miform">
+    	Usuario:
+    	<input type="text" maxlength="11" size="10" name="usuario" id=usuario></br>
+    	Password:
+    	<input type="password" maxlength="20" size="10" name="password" id=password></br>
+    	<input type="button" name="enviar" value="enviar" onClick="getData();">
+    	<div id="contenido"></div>
+	</form>
+	</div>
+"""	
 
     # <div class="login">
     #     Usuario: 
@@ -30,7 +57,7 @@ def login():
     #     <input type="submit" name="enviar" value="Ingresar">
     # </div>
 
-def header():
+def header(valor):
     print"""
     <body>
     <header>
@@ -38,11 +65,20 @@ def header():
             <a href="inicio.py"><img src="logo.png" alt="iniGo"></a>
         </div>
     """
-    login()
+    if (valor == False):
+        login()
+    else:
+        print  """
+    <div class=login>
+    <form action="inicio.py">
+        <input type="submit" value="Cerrar Sesion">
+    </form>
+    </div>
+""" 
+
     print """
     </header>
     """
-
     
 def topbar():
     print"""
